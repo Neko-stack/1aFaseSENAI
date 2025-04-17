12f3b61fda575a5631843834e388dee94562acd1
Neko-stack <gabriel.evil27@gmail.com>
2025-04-17 20:38:05 -0300

    ExercicioJS

---
+++ JS e HTML/00-primeiro-js/script.js
@@ -0,0 +1,4 @@
+function tratarBotao(){
+    alert("Clicou muito bem!")
+}
+
---
+++ JS e HTML/Exercicios/exercicio.js
@@ -0,0 +1,46 @@
+function somar(){
+    let n1,n2,soma
+
+    n1 = Number(prompt("Primeiro número: "))
+    n2 = Number(prompt("Segundo número: "))
+
+    soma = n1 + n2
+    
+    alert("Soma: " + soma)
+}
+
+function diminuir(){
+    let n1, n2, diminuir
+
+    n1 = Number(prompt("Primeiro número:"))
+    n2 = Number(prompt("Segundo número: "))
+
+    diminuir = n1 - n2 
+
+    alert("Subtração: "+ diminuir)
+
+}
+
+function dividir(){
+    let n1, n2, dividir
+
+    n1 = Number(prompt("Primeiro número: "))
+    n2 = Number(prompt("Segundo número: "))
+
+    dividir = n1 / n2 
+
+    alert("Divição: "+ dividir)
+
+}
+
+function multiplicar(){
+    let n1, n2, multiplicar
+
+    n1 = Number(prompt("Primeiro número: "))
+    n2 = Number(prompt("Segundo número: "))
+
+    multiplicar = n1 * n2 
+
+    alert("Divição: " + multiplicar)
+
+}
